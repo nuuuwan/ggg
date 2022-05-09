15a28c5194e72c559cba07a719e7d283536028e0
nuuuwan <nuuuwan@gmail.com>
2022-05-09 18:04:35 +0530

    Moved testing functions

---
+++ testing_only.py
@@ -0,0 +1,7 @@
+import json
+
+def test_tweet(api):
+    id = 1523622701159825416
+    tweet = api.get_status(id, tweet_mode='extended')
+    print(json.dumps(tweet._json, indent=2))
+    print(get_video_url_list(tweet))
