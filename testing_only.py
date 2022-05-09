20bc15e481611aad0d9134f3a0e5cf957badf503
nuuuwan <nuuuwan@gmail.com>
2022-05-09 18:24:16 +0530

    Deleted

--- testing_only.py
+++
@@ -1,8 +0,0 @@
-import json
-
-
-def test_tweet(api):
-    id = 1523622701159825416
-    tweet = api.get_status(id, tweet_mode='extended')
-    print(json.dumps(tweet._json, indent=2))
-    print(get_video_url_list(tweet))
